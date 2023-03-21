0131f23f20769f0efd3a70a1a73f7e6315aad395
Jacob <jakesoto48@gmail.com>
2023-03-21 17:35:59 -0500

    Update tsh.c

--- src/tsh.c
+++ src/tsh.c
@@ -1,20 +1,33 @@
-#include <sys/types.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#include <stdlib.h>
 
-int main() {
-    pid_t pid;
-    string path = "/bin";
+int main(int argc, char *argv[]) {
+    FILE *infile;
+    char *buffer = NULL;
+    size_t bufferSize = 0;
+    ssize_t charCount;
+    //String path = "/bin";
     // use access to ensure the system call will succeed
     //try 1. /bin/''
     //2 /usr/bin/''
     //3 ./''
 
-    while(1) {
-        fprintf("tsh> ");
-        getline();
+    infile = fopen(argv[1], "r");
+    if (infile == NULL) {
+        perror("Couldn't open file");
+        exit(1);
     }
 
+    while(1) {
+        printf("tsh> ");
+        charCount = getline(&buffer, &bufferSize, infile);
+        if (charCount < 0) {
+            printf("error");
+        }
+        printf("Num of characters read in: %s\n", charCount);
+    }
+
+    free(buffer);
+    fclose(infile);
     return 0;
 }
