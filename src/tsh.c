6e0a25f0274e33296a492e92ac35318cc2de4f3b
Jacob <jakesoto48@gmail.com>
2023-03-27 01:35:28 -0500

    Update tsh.c

--- src/tsh.c
+++ src/tsh.c
@@ -6,9 +6,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+char *myInitials = "JS";
 char ** getArgsList(char *buffer);
 bool cmdChecker(char *argsList[]);
 void executeCommand(char *argsList[]);
+
 
 int main(int argc, char *argv[]) {
     FILE *infile;
@@ -33,9 +35,16 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+/* Prints error message with the user's initials */
+void printErrorMessage(char *userInitials) {
+    char error_message[30] = "An error has occurred (from ";
+    strcat(error_message, userInitials);
+    strcat(error_message, ")\n");
+    write(STDERR_FILENO, error_message, strlen(error_message));
+}
 
 /*  Returns character array containing arguments
-    from input seperated by a space */
+    from input seperated by white space */
 char ** getArgsList(char *buffer) {
     int i = 0;
     static char *argsList[50];
@@ -58,11 +67,20 @@ bool cmdChecker(char *argsList[]) {
     while (argsList[i] != NULL) {
         for (int j = 0; j < 4; j++) {
             if (strcmp(argsList[i], commands[j]) == 0) {
-                if (j == 0)
+                if (j == 0)             // exit
                     exit(0);
-                else if (j == 1) {
+                else if (j == 1) {      // change directory
                     if (chdir(argsList[1]) == -1)
-                        perror("Failed to change directories");
+                        printErrorMessage(myInitials);
+                    return true;
+                }
+                else if (j == 2) {      // path
+                    char path[1024];
+                    if (argsList[i+2] == NULL) {
+                        if (getcwd(path, sizeof(path)) == NULL)
+                            printErrorMessage(myInitials);
+                        printf("Path is set to: %s\n", path);
+                    }
                     return true;
                 }
             }
@@ -77,23 +95,16 @@ bool cmdChecker(char *argsList[]) {
 void executeCommand(char *argsList[]) {
     pid_t pid = fork();
 
-    if (pid < 0) {
-        fprintf(stderr,"Fork Failed\n");
-		exit(0);
-    }
+    if (pid < 0)
+        printErrorMessage(myInitials);
     else if (pid == 0) {    // Child process
         int execStatus = execvp(argsList[0], argsList);
-        if (execStatus == -1) {
-            fprintf(stderr,"Exec Failed\n");
-		    exit(0);
-        }
+        if (execStatus == -1)
+            printErrorMessage(myInitials);
     }
     else {                  // Parent process
         pid_t pid2 = wait(NULL);
-        if (pid2 < 0) {
-            fprintf(stderr,"Wait Failed\n");
-		    exit(0);
-        }
-        fflush(stderr);
+        if (pid2 < 0)
+            printErrorMessage(myInitials);
     }
 }
